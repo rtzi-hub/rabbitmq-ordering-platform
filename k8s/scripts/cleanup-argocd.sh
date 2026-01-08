b586902497df26542210211da10d2a199b15129e
Ron Tzidky <rontzidky@gmail.com>
2026-01-08 19:59:23 +0200

    Hidding Readiness for argocd testing

--- k8s/scripts/cleanup-argocd.sh
+++ k8s/scripts/cleanup-argocd.sh
@@ -1,2 +1,13 @@
 helm uninstall argocd -n default
 kubectl delete crd -n default appprojects.argoproj.io  applicationsets.argoproj.io  applications.argoproj.io
+
+
+# Deletation option
+```bash
+kubectl get applications.argoproj.io -n argocd -o name | while read app; do
+  kubectl patch "$app" -n argocd --type=json -p='[{"op":"remove","path":"/metadata/finalizers"}]' || true
+done
+
+kubectl delete ns argocd
+kubectl get ns argocd
+```
